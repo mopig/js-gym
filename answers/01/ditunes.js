6f9d70a9cb9b55a903d3c87d0cf172c570a32abb
ditunes <oldhomelh@sina.com>
2016-09-09 00:46:17 +0800

    提交rang作业
    做了两种实现，本质上还是很厌恶if

---
+++ answers/01/ditunes.js
@@ -0,0 +1,35 @@
+// array has internal defined iterator
+function rangForMapToArray(input){
+    return Object.keys(input).map((key)=>{
+        return [key,input[key]]
+    });
+}
+
+
+function rangeForGenerator(input){
+    function* generator(){
+      //array [2,3,4] keys is [0,1,2] and object {"a","b"} keys is ["a"] 
+      var inputKeys = Object.keys(input); 
+      for(let index in inputKeys){
+         var key = inputKeys[index] 
+         yield [key, input[key]];
+      }
+    }
+    return generator();
+}
+
+
+
+function test(input,method){
+  for(let [k,v] of method(input)){
+    console.log(`${k}`+"=>"+`${v}`);
+  } 
+}
+
+console.log("------generator----")
+test([1,2,5],rangeForGenerator)
+test({"ad":"sd"},rangeForGenerator)
+
+console.log("---------array---------")
+test([1,2,3,4],rangForMapToArray)
+test({"a":"b","c":"d"},rangForMapToArray);
